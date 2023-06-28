66e0943642878e767746407ace45b696e0e32cb7
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-28 15:06:25 +0100

    AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
+
 export default {
   data() {
     return {
     }
-  }
+  },
+  components: { AppTitle, }
 }
 </script>
 
-<template></template>
+<template>
+  <!-- Header-->
+  <AppTitle />
+</template>
 
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style>
+h1 {
+    color: red;
+    text-align: center;
+    padding: 30px 0;
+}
+</style>
