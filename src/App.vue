1eb5da037b5bb900f47e34cabe8e59595ca0b7d9
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-28 15:40:00 +0100

    AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   data() {
     return {
     }
   },
-  components: { AppTitle, }
+  components: { AppTitle, AppHero }
 }
 </script>
 
 <template>
   <!-- Header-->
   <AppTitle />
+
+  <!-- Jumbotron -->
+  <AppHero />
 </template>
 
 <style></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- jumbotron -->
+    <figure>
+        <img src="../assets/img/jumbo.png" alt="jumbo">
+    </figure>
+</template>
+
+<style>
+img {
+    max-width: 100%;
+    height: auto;
+}
+</style>
